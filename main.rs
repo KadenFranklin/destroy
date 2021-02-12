112b714740a8016fc6a53318d358b73e32dad52b
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2021-02-12 12:44:13 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,14 +2,9 @@ use std::env;
 use std::fs;
 
 fn main() -> std::io::Result<()> {
-    for arg in env::args().skip(1) {
-        for entry in fs::read_dir(".")? {
-            let dir = entry?;
-            if dir.path().to_string_lossy() == arg {
-                fs::remove_file(dir.path())?;
-                println! ("{} has been deleted", arg);
-            }
-        }
-    }
+    let args : Vec<String> =env::args().collect();
+    let var = &args[1];
+    fs::remove_file(var)?;
+    println! ("{} has been deleted", var);
     Ok(())
 }
